3af9026ad78ff9f7e05b31be40e68707224ffcf5
Arun3sh <m.aruneshwar@gmail.com>
2021-12-18 10:18:18 +0530

    features added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,45 @@
 import { TextField } from '@mui/material';
 import { FaSearch } from 'react-icons/fa';
-// import { SearchIcon } from '@mui/icons-material/Search';
+import Badge from '@mui/material/Badge';
+import Notification from '@mui/icons-material/Notifications';
+import MailIcon from '@mui/icons-material/Mail';
+import { Avatar, Divider, Button } from '@mui/material';
 
 export function Header() {
 	return (
 		<header className="content-header">
-			{/* <SearchIcon /> */}
+			{/* Search Bar /> */}
 
 			<div className="search-bar">
 				<TextField
 					id="outlined-basic"
-					placeholder="Please type your search keyword.."
+					placeholder="Search here..."
 					variant="outlined"
-					fullWidth={true}
+					className="text-field"
 				/>
-				<FaSearch />
+				<Button className="search-button">
+					<FaSearch className="search-icon" color="primary" />
+				</Button>
+			</div>
+
+			{/* Icon and user profile holder */}
+			<div className="icon-holder">
+				{/* Notification */}
+				<Badge badgeContent={4} color="error" className="badge">
+					<Notification color="action" />
+				</Badge>
+
+				{/* Mail */}
+				<Badge badgeContent={5} color="error" className="badge">
+					<MailIcon color="action" />
+				</Badge>
+				<Divider orientation="vertical" flexItem />
+
+				{/* User name and profie pic */}
+				<div className="user-badge">
+					<span className="profile-name">UserName</span>
+					<Avatar src="/broken-image.jpg" />
+				</div>
 			</div>
 		</header>
 	);
