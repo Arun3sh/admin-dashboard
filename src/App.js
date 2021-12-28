e40044d48bfcab2310c23191a1996dce7546b46d
Arun3sh <m.aruneshwar@gmail.com>
2021-12-29 00:44:41 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import { SidemenuBar } from './components/SidemenuBar';
-import { Contentwrapper } from './components/Contentwrapper';
+import { Header } from './components/Header';
+import { Maincontent } from './components/Maincontent';
+import { Error } from './components/ErrorPage';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
 			<div className="Wrapper">
 				<SidemenuBar />
-				<Contentwrapper />
+				<div className="content-wrapper">
+					<Header />
+					<Switch>
+						<Route exact path="/">
+							<Maincontent />
+						</Route>
+						<Route path="**">
+							<Error />
+						</Route>
+						<Route path="/chart">Charts</Route>
+					</Switch>
+					<footer>Copyright © Your Website 2020</footer>
+				</div>
 			</div>
 		</div>
 	);
