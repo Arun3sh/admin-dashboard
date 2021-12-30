10b883c47d7023574c998901e0abd5875ade9008
Arun3sh <m.aruneshwar@gmail.com>
2021-12-30 23:52:54 +0530

    props added

--- src/components/Maincontent.js
+++ src/components/Maincontent.js
@@ -3,7 +3,7 @@ import { PictorialData } from './PictorialData';
 import { Othercontent } from './Othercontent';
 import { Button } from '@mui/material';
 
-export function Maincontent() {
+export function Maincontent({ menu, setMenu }) {
 	return (
 		<div className="maincontent">
 			<div className="first-row">
@@ -13,7 +13,7 @@ export function Maincontent() {
 				</Button>
 			</div>
 			<Reportdata />
-			<PictorialData />
+			<PictorialData menu={menu} setMenu={setMenu} />
 			<Othercontent />
 		</div>
 	);
