2d7f7a4f37f5532196c9b54f17a7cc8279000539
Arun3sh <m.aruneshwar@gmail.com>
2021-12-30 23:51:08 +0530

    hamburger menu added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,60 @@ import Badge from '@mui/material/Badge';
 import Notification from '@mui/icons-material/Notifications';
 import MailIcon from '@mui/icons-material/Mail';
 import { Avatar, Divider, Button } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from 'react';
 
-export function Header() {
+export function Header({ menu, setMenu }) {
+	const [showsearch, setShowsearch] = useState(false);
 	return (
 		<header className="content-header">
-			{/* Search Bar /> */}
+			<div className="content-header-div">
+				{/* Hamburger Menu */}
 
-			<div className="search-bar">
+				<Button className="hamburger-menu" onClick={() => setMenu(!menu)}>
+					<MenuIcon />
+				</Button>
+
+				{/* Search Bar /> */}
+
+				<div className="search-bar">
+					<TextField
+						id="outlined-basic"
+						placeholder="Search here..."
+						variant="outlined"
+						className="text-field"
+					/>
+					<Button className="search-button">
+						<FaSearch className="search-icon" color="primary" />
+					</Button>
+				</div>
+
+				<Button className="search-button-small" onClick={() => setShowsearch(!showsearch)}>
+					<FaSearch className="search-icon" color="primary" />
+				</Button>
+
+				{/* Icon and user profile holder */}
+				<div className="icon-holder">
+					{/* Notification */}
+					<Badge badgeContent={4} color="error" className="badge">
+						<Notification color="action" />
+					</Badge>
+
+					{/* Mail */}
+					<Badge badgeContent={5} color="error" className="badge">
+						<MailIcon color="action" />
+					</Badge>
+					<Divider orientation="vertical" flexItem />
+
+					{/* User name and profie pic */}
+					<div className="user-badge">
+						<span className="profile-name">UserName</span>
+						<Avatar src="/broken-image.jpg" />
+					</div>
+				</div>
+			</div>
+
+			<div className="search-bar-big" style={{ display: showsearch ? 'flex' : 'none' }}>
 				<TextField
 					id="outlined-basic"
 					placeholder="Search here..."
@@ -20,26 +67,6 @@ export function Header() {
 				<Button className="search-button">
 					<FaSearch className="search-icon" color="primary" />
 				</Button>
-			</div>
-
-			{/* Icon and user profile holder */}
-			<div className="icon-holder">
-				{/* Notification */}
-				<Badge badgeContent={4} color="error" className="badge">
-					<Notification color="action" />
-				</Badge>
-
-				{/* Mail */}
-				<Badge badgeContent={5} color="error" className="badge">
-					<MailIcon color="action" />
-				</Badge>
-				<Divider orientation="vertical" flexItem />
-
-				{/* User name and profie pic */}
-				<div className="user-badge">
-					<span className="profile-name">UserName</span>
-					<Avatar src="/broken-image.jpg" />
-				</div>
 			</div>
 		</header>
 	);
