1cbffcef432ecb7df60a7cecbe2dbfeb350d7e56
Arun3sh <m.aruneshwar@gmail.com>
2022-01-14 23:52:48 +0530

    user details used

--- src/user components/Viewprofile.js
+++ src/user components/Viewprofile.js
@@ -3,8 +3,10 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
+import { useHistory } from 'react-router-dom';
 
-export function Viewprofile() {
+export function Viewprofile({ name, email, profilepic, food, sport, hobby, location, language }) {
+	const history = useHistory();
 	return (
 		<div className="viewprofile-wrapper">
 			{/* <Avatar
@@ -14,22 +16,18 @@ export function Viewprofile() {
 			/> */}
 			<Card sx={{ maxWidth: 345 }}>
 				<CardActionArea>
-					<Avatar
-						className="profile-pic"
-						alt="Travis Howard"
-						src="https://www.themoviedb.org/t/p/w1280//zHJYRHI5HkQuwLW5KyeXGQ596u7.jpg"
-					/>
+					<Avatar className="profile-pic" alt={name} src={profilepic} />
 					<CardContent>
 						<Typography gutterBottom variant="h5" component="div">
-							User Name
+							{name}
 						</Typography>
 						<Typography variant="body2" color="text.secondary">
-							lorem ipsum
+							{email}
 						</Typography>
 					</CardContent>
 				</CardActionArea>
 				<CardActions>
-					<Button size="small" color="primary">
+					<Button size="small" color="primary" onClick={() => history.goBack()}>
 						Back
 					</Button>
 					<Button size="small" color="primary">
