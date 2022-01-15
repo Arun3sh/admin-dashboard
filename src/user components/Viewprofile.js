ad984aefd06a397b2634da60e9d33f559c27bcaf
Arun3sh <m.aruneshwar@gmail.com>
2022-01-15 23:55:18 +0530

    profile edit

--- src/user components/Viewprofile.js
+++ src/user components/Viewprofile.js
@@ -1,40 +1,46 @@
 import { Avatar } from '@mui/material';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import { Button } from '@mui/material';
 import { useHistory } from 'react-router-dom';
+import { Mail } from '@mui/icons-material';
+import { blue } from '@mui/material/colors';
 
-export function Viewprofile({ name, email, profilepic, food, sport, hobby, location, language }) {
+export function Viewprofile({
+	name,
+	email,
+	about,
+	profilepic,
+	coverpic,
+	food,
+	sport,
+	hobby,
+	location,
+	language,
+}) {
 	const history = useHistory();
 	return (
 		<div className="viewprofile-wrapper">
-			{/* <Avatar
-				className="profile-pic"
-				alt="Travis Howard"
-				src="https://www.themoviedb.org/t/p/w1280//zHJYRHI5HkQuwLW5KyeXGQ596u7.jpg"
-			/> */}
-			<Card sx={{ maxWidth: 345 }}>
-				<CardActionArea>
+			<div className="user-main">
+				<div className="user-main-wrapper">
+					<h3>{name}</h3>
 					<Avatar className="profile-pic" alt={name} src={profilepic} />
-					<CardContent>
-						<Typography gutterBottom variant="h5" component="div">
-							{name}
-						</Typography>
-						<Typography variant="body2" color="text.secondary">
-							{email}
-						</Typography>
-					</CardContent>
-				</CardActionArea>
-				<CardActions>
-					<Button size="small" color="primary" onClick={() => history.goBack()}>
-						Back
-					</Button>
-					<Button size="small" color="primary">
-						Edit Profile
-					</Button>
-				</CardActions>
-			</Card>
+					<p className="about-user">{about}</p>
+					<p className="location">{location}</p>
+					<Avatar sx={{ bgcolor: blue[500] }} className="mail-user">
+						<Mail />
+					</Avatar>
+					<div className="btns-user">
+						<Button size="small" color="primary" onClick={() => history.goBack()}>
+							Back
+						</Button>
+						<Button size="small" color="primary">
+							Edit Profile
+						</Button>
+					</div>
+				</div>
+			</div>
+			<div className="user-other">
+				<div className="">Hobby</div>
+			</div>
 		</div>
 	);
 }
